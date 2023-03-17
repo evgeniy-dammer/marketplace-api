3628fcc0efde4b5e97644bd17c0c86258889d7a6
Evgeniy Dammer <evgeniydammer@gmail.com>
2023-03-17 11:18:43 +0500

    Github Actions Test 1
    Github Actions Test 1

--- pkg/queryparameter/type.go
+++ pkg/queryparameter/type.go
@@ -3,11 +3,13 @@ package queryparameter
 import (
 	"github.com/evgeniy-dammer/marketplace-api/pkg/pagination"
 	"github.com/evgeniy-dammer/marketplace-api/pkg/sort"
+	"time"
 )
 
 type QueryParameter struct {
 	Search     string
 	Sorts      sort.Sorts
 	Pagination pagination.Pagination
-	/*Тут можно добавить фильтр*/
+	StartDate  time.Time
+	EndDate    time.Time
 }
